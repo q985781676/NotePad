82d392dd380121abad47fd02d19e36a4a21ae356
zxy <zxy@qq.com>
2020-05-18 19:04:27 +0800

    二次修正NotePad在新版本中的bug

--- app/src/main/java/com/example/android/notepad/NoteEditor.java
+++ app/src/main/java/com/example/android/notepad/NoteEditor.java
@@ -38,6 +38,8 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.widget.EditText;
 
+import androidx.appcompat.widget.AppCompatEditText;
+
 /**
  * This Activity handles "editing" a note, where editing is responding to
  * {@link Intent#ACTION_VIEW} (request to view data), edit a note
@@ -81,7 +83,7 @@ public class NoteEditor extends Activity {
     /**
      * Defines a custom EditText View that draws lines between each line of text that is displayed.
      */
-    public static class LinedEditText extends EditText {
+    public static class LinedEditText extends AppCompatEditText {
         private Rect mRect;
         private Paint mPaint;
 
